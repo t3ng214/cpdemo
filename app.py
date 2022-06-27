25e2d7e886cb79f0d96568f1d985752ac0f32ab6
teng.guo214@gmail.com <m2o1n4key>
2022-06-27 11:46:47 +0800

    demo update

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-"""Main application file"""
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/<random_string>')
-def returnBackwardsString(random_string):
-    """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='127.0.0.1', port=8080)
